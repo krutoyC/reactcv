bd5bc6934ece47ab29ee9eed30d0bdde202eee32
Carlos M. Rodriguez <carlosnotengo1@gmail.com>
2021-02-10 02:28:11 -0300

    se agrego font awesome

--- src/common/Header.js
+++ src/common/Header.js
@@ -2,29 +2,43 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import {FontawesomeIcom} from "@fortawesome/react-fontawesome";
-import {faAtom} from "@fortawesome/free-solid-svg-icons";
+import { BsFillLightningFill } from "react-icons/bs";
+
 const Header = () => {
   return (
-    <Navbar bg="dark" expand="dark" className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <Navbar.Brand href="#home">
-    {/* <div style={{fontSize:"10em", color:"white"}}><FontawesomeIcom icon={faAtom}/></div> */}
-    <h5>React-Bootstrap</h5>
-    </Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="ml-auto">
-      <Nav.Link href="#home" className="active">Inicio</Nav.Link>
-      <Nav.Link href="#link" className="active">Admin</Nav.Link>
-      <NavDropdown title="Categorias" id="basic-nav-dropdown" className="active">
-        <NavDropdown.Item href="#action/3.1">Datos </NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Estudios</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Experiencia</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-    
+    <Navbar
+      bg="dark"
+      expand="dark"
+      className="navbar navbar-expand-lg navbar-dark bg-dark"
+      fixed
+
+    >
+      <Navbar.Brand href="/">
+        <h5 style={{ fontSize: "2em", color: "white" }}>
+          <BsFillLightningFill />
+        </h5>
+      </Navbar.Brand >
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto tex-bold" style={{fontSize:"1em"}}>
+          <Nav.Link href="#home" className="active">
+            Inicio
+          </Nav.Link>
+          <Nav.Link href="#link" className="active">
+            Admin
+          </Nav.Link>
+          <NavDropdown
+            title="Categorias"
+            id="basic-nav-dropdown"
+            className="active"
+          >
+            <NavDropdown.Item href="#action/3.1">Datos </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Estudios</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Experiencia</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
